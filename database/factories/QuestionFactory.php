884deba25b3cb1fb9c6eb9cb30485782f1d4c157
ashraf salem <eng.ashrafsalem@gmail.com>
2020-01-31 20:24:53 +0200

    alter migrations and rearrange them

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -4,9 +4,21 @@
 
 use App\Models\Question;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use App\Models\category;
+use App\User;
 
 $factory->define(Question::class, function (Faker $faker) {
+    $title = $faker->sentence;
     return [
-        //
+        'title' => $title,
+        'slug' => Str::slug($title),
+        'body' => $faker->text,
+        'category_id' => function(){
+            return Category::all()->random();
+        },
+        'user_id' => function(){
+            return User::all()->random();
+        }
     ];
 });
