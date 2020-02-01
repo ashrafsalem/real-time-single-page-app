ea2e717fc08855d80b808df19c1ed718b9a0d58b
ashraf salem <eng.ashrafsalem@gmail.com>
2020-02-02 01:28:02 +0200

    question and ca

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -4,82 +4,40 @@ namespace App\Http\Controllers;
 
 use App\Models\Question;
 use Illuminate\Http\Request;
+use App\Http\Resources\QuestionResource;
+use Symfony\Component\HttpFoundation\Response;
 
 class QuestionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        //
+        return  QuestionResource::collection(Question::latest()->get());
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+//        auth()->user()->qustions()->create($request->all());
+        Question::create($request->all());
+        return response("created", Response::HTTP_CREATED);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Question  $question
-     * @return \Illuminate\Http\Response
-     */
     public function show(Question $question)
     {
-        //
+        return new QuestionResource($question);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Question  $question
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Question $question)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Question  $question
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Question $question)
     {
-        //
+        $question->update($request->all());
+        return response('updated', Response::HTTP_ACCEPTED);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Question  $question
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
--- app/Models/Question.php
+++ app/Models/Question.php
@@ -8,6 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
 
+    protected $guarded = [];
+
+    // search key, change the way of search,
+    // provide the key i want to search with
+    // the function must be like this , getRouteKeyName,
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
     // reverse relation with user
     public function user()
     {
@@ -24,5 +33,11 @@ class Question extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    // create new path field with the format of getter getPathAttribute
+    public function getPathAttribute()
+    {
+        return asset("api/questions/$this->slug");
     }
 }
